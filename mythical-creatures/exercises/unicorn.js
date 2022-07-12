952eb862675f1e078f4a7b6195ec3b2c00abf025
Beth Wilson <bethwilson777@gmail.com>
2022-07-11 19:18:45 -0600

    Initial commit

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,24 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+  //  this.color = color;  removed this because it came before the default white return below and failed the test on line 34.
+    console.log(color)
+    if (color === undefined) {
+      return this.color = 'white'
+    } else {
+      return this.color = color;
+    }
   }
-}
+    isWhite() {
+    if (this.color !== 'white') {
+      return false
+    }
+  }
+    says(statement) {
+      return `**;* ${statement} *;**`
+    }
+  }
+
+
 
 module.exports = Unicorn;
